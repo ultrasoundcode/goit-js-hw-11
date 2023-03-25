41918e4803717fca8ac278ae1c59f67e75871ddb
Askhat Anarbayev <0x.ultrasound@gmail.com>
2023-03-25 17:28:50 +0600

    update
    update index.js, index.html, package.json and  package-lock.json

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import './css/styles.css';
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const Lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
+Notiflix.Notify.init({ width: '450px' });
 
-const DEBOUNCE_DELAY = 300;
+const formEl = document.querySelector('#search-form');
+const galleryEl = document.querySelector('.gallery');
+const btnSearchEl = document.querySelector('.search-button');
+const btnLoadMoreEl = document.querySelector('.load-more');
+btnLoadMoreEl.classList.add('invisible');
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API = '34722536-bec3ddb016f82f379d3e29af6';
